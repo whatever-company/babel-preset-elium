e5304f3d7a5353db82bd48b18bc5ef38fa809483
Quentin Gérôme <geromequentin@gmail.com>
2020-10-26 22:37:20 +0100

    Revert "chore: set preset-react's runtime in automatic mode (used by react v17)"
    This reverts commit b3fad9f531c8a7b231921b4f44ac204fb063293d.

--- create.js
+++ create.js
@@ -47,7 +47,6 @@ module.exports = function(api, opts, env) {
 			[
 				require('@babel/preset-react').default,
 				{
-					runtime: "automatic",
 					// Adds component stack to warning messages
 					// Adds __self attribute to JSX which React will use for some warnings
 					development: isEnvDevelopment,
